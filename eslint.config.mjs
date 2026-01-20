3e95fd97effe955cc645edf5192a1e03468b0707
TahaHanif2424 <tahahanif24@gmail.com>
2026-01-20 13:17:48 +0500

    updated enlist to fix all the errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,6 +32,16 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-return': 'warn',
       '@typescript-eslint/no-unsafe-call': 'warn',
       '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+    },
+  },
+  {
+    files: ['**/prisma/**/*.ts', '**/*.service.ts', '**/functions.ts'],
+    rules: {
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
     },
   },
 );
